a7b5fd75166e3d2a4c7d260bc0cdb7a49f083f29
alcaoddys <36655853+alcaoddys@users.noreply.github.com>
2018-02-21 12:47:05 +0700

    Update linebot.py

--- linebot.py
+++ linebot.py
@@ -1834,17 +1834,17 @@ def bot(op):
 #------------------------------------------------------------------------------------
 	if 'MENTION' in msg.contentMetadata.keys() != None:
                  	if wait["detectMention"] == True:
-                     	contact = acil.getContact(msg.from_)
-                     	cName = contact.displayName
-                     	balas = ["",cName + " what ?, ", cName + " Kenapa? pc dia aja klo penting,  " + cName + "?", "Ada Perlu apa? jgn tag doang, " + cName + "?","Hmm?, ", "Jgn tag tag ah, "]
-                     	ret_ = "." + random.choice(balas)
-                     	name = re.findall(r'@(\w+)', msg.text)
-                     	mention = ast.literal_eval(msg.contentMetadata['MENTION'])
-                     	mentionees = mention['MENTIONEES']
-                     	for mention in mentionees:
-                           	if mention['M'] in Bots:
-                                  	acil.sendText(msg.to,ret_)
-                                  	break    
+                     		contact = acil.getContact(msg.from_)
+                     		cName = contact.displayName
+                     		balas = ["",cName + " what ?, ", cName + " Kenapa? pc dia aja klo penting,  " + cName + "?", "Ada Perlu apa? jgn tag doang, " + cName + "?","Hmm?, ", "Jgn tag tag ah, "]
+                     		ret_ = "." + random.choice(balas)
+                     		name = re.findall(r'@(\w+)', msg.text)
+                     		mention = ast.literal_eval(msg.contentMetadata['MENTION'])
+                     		mentionees = mention['MENTIONEES']
+                     		for mention in mentionees:
+                           		if mention['M'] in Bots:
+                                  		acil.sendText(msg.to,ret_)
+                                  		break    
 #------------------------------------------------------------------------------------
         if op.type == 55:
             if op.param1 in wait2['readPoint']:
