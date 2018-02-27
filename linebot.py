ea6007d38fb0706a4d2d16c24745c9d084fe114e
alcaoddys <36655853+alcaoddys@users.noreply.github.com>
2018-02-28 02:21:29 +0700

    Update linebot.py

--- linebot.py
+++ linebot.py
@@ -1822,10 +1822,10 @@ def bot(op):
              #membermid = [contact.mid for contact in group.invitee]
                     random.choice(KAC).kickoutFromGroup(op.param1,[ contact.mid for contact in group.invitee])
                     random.choice(KAC).sendText(op.param1, "Mau Ngundang Siapa Ka?\nKk Bukan Admin\nJadi Aku Cancel😛")
-        else:
-            acil.sendText(op.param1,"")
-        else:
-        acil.sendText(op.param1,"")
+                    else:
+                    acil.sendText(op.param1,"")
+                    else:
+                    acil.sendText(op.param1,"")
 	    if op.param2 not in Bots:
 		if op.param2 in Bots:
 		    pass
